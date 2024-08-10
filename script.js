c268b9855e6a6707ed9c63e6026cb966e9711d06
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-08-10 17:10:43 +0800

    js finish

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ menuBtn.addEventListener('click', () => {
     sidebar.style.left = "0px";
 });
 
+document.addEventListener('click', (event) => {
+    const isClickInside = sidebar.contains(event.target) || menuBtn.contains(event.target);
+
+    if (!isClickInside) {
+        sidebar.style.left = "-172px";
+    }
+});
